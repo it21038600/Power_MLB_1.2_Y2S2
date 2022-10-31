7288654a916d72bb6c74bb50ef7bb54984e68286
Rajitha P.A.S <87379779+it21038600@users.noreply.github.com>
2022-10-31 13:32:03 +0530

    finished project

--- app/src/main/java/com/example/loginregister/Register.java
+++ app/src/main/java/com/example/loginregister/Register.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+//This is my Register page
+
 public class Register extends AppCompatActivity {
 
     @Override
